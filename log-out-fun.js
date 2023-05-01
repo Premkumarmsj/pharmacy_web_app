6a10009fae69d613f1a7a40c7061aa06011eff1c
Premkumarmsj <121605436+Premkumarmsj@users.noreply.github.com>
2023-05-01 19:08:26 +0530

    Delete log-out-fun.js

--- log-out-fun.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var logOut = document.getElementById('logout');
-    logOut.onclick = function(){
-        alert("logout Successfully")
-        localStorage.setItem('isLogedIn',false);
-        localStorage.removeItem('userInfo');
-    }
-})
